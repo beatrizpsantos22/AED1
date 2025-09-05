153634efe4fd6a646753b563d58da1e0b387669d
beatrizpsantos22 <bpsantos022@gmail.com>
2025-09-04 22:27:02 -0300

    Create 1180b.c

---
+++ 1180b.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+ 
+int main() {
+   int n, i, posicao;
+   int *p;
+   
+   scanf("%d", &n);
+   p = (int*)malloc(n*sizeof(int));
+   
+   if (p == NULL){
+       printf("Erro ao alocar memória\n");
+       return 1;
+   }
+   
+   for (i = 0; i < n; i++){
+       scanf("%d", (p+1));
+   }
+   int menor = *p;
+   posicao = 0;
+   
+   for (i = 1; i < n; i++){
+       if(*(p+i) < menor){
+           menor = *(p+i);
+           posicao = i;
+       }
+   }
+   printf("Menor valor: %d\n", menor);
+   printf("Posição: %d\n", posicao);
+   
+   free(p);
+   
+    return 0;
+}
