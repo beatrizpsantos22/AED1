90b43e4b3c825a8661e1b7f7a689dc26b1825500
beatrizpsantos22 <bpsantos022@gmail.com>
2025-12-07 16:13:31 -0300

    Create 1144.c

---
+++ 1144.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) {
+        int i2 = i * i;
+        int i3 = i * i * i;
+
+        printf("%d %d %d\n", i, i2, i3);
+        printf("%d %d %d\n", i, i2 + 1, i3 + 1);
+    }
+
+    return 0;
+}
